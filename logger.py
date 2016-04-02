a214b03ec309b11005819cdc6feae1e1c27fb3aa
AgentBrown <agentbrown@land.ru>
2016-04-02 14:54:51 +0000

    Added time to logger

--- logger.py
+++ logger.py
@@ -1,12 +1,18 @@
+from datetime import datetime as dt
+
+
 LOG_NAME = 'bot.log'
 LOG_FILE = open(LOG_NAME, 'a')
 
+def curr_time():
+    return '{}:{}:{}'.format(dt.now().hour, dt.now().minute, dt.now().second)
+
 def info(message):
-    print('[INFO] ' + message, file=LOG_FILE)
+    print(curr_time() + ' [INFO] ' + message, file=LOG_FILE)
 
 def warning(message):
-    print('[WARN] ' + message, file=LOG_FILE)
+    print(curr_time() + ' [WARN] ' + message, file=LOG_FILE)
 
 def error(message):
-    print('[ERR!] ' + message, file=LOG_FILE)
+    print(curr_time() + ' [ERR!] ' + message, file=LOG_FILE)
 
