9c568889f6f8c259bbf362d92de28a339aa202dc
MayBee <mbee@protonmail.ch>
2016-04-03 02:33:17 +0300

    Added search and delete subcommands

--- modules/joke.py
+++ modules/joke.py
@@ -29,7 +29,6 @@ def run(message):
         logger.info("Config not found, created default")
     except:
         logger.error("Uncaught error:")
-        logger.error(traceback.format_exc())
         return "Sorry, an error occured"
 
     msg = message["text"].split(" ")
@@ -45,8 +44,38 @@ def run(message):
             else:
                 logger.info("Add request denied")
                 return "Sorry, adding jokes is enabled only for admins"
-        elif msg[1] == "somesubcommand":
-            pass  # WIP
+        elif msg[1] == "search":
+            logger.info("Search request...")
+            sub = " ".join(msg[2:])
+            answer = ""
+            ctr = 0
+            for i in range(len(data["jokes"])):
+                if sub.lower() in data["jokes"][i].lower():
+                    ctr += 1
+                    answer = "{}{}: {}\n".format(answer, i, data["jokes"][i])
+            logger.info("Found {} results, sending...".format(ctr))
+            return answer.rstrip("\n")
+            # crazy pythonista way
+            # return "\n".join([str(data["jokes"].index(s)) + ": " + s for s in data["jokes"] if " ".join(msg[2:]).lower() in s.lower()])
+        elif msg[1] == "delete":
+            logger.info("Deletion request...")
+            if message["from"]["id"] in data["admins"]:
+                try:
+                    index = int(" ".join(msg[2:]))
+                    joke = data["jokes"].pop(index)
+                except ValueError:
+                    logger.info("Invalid number")
+                    return "Hey, looks like NaN!"
+                except IndexError:
+                    logger.info("Invalid number")
+                    return "I do not have SO many jokes yet"
+                with open("modules/joke.json", "w", encoding="utf-8") as fl:
+                    fl.write(json.dumps(data))
+                logger.info("Deletion request granted")
+                return "Joke \"{}\" removed!".format(joke)
+            else:
+                return "Sorry, deleting jokes is only for admins"
+
         else:
             logger.info("Unknown subcommand, ignoring")
             return random.choice(data["jokes"])
