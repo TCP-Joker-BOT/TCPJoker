4bddcda9876259837511959d7a57edc662f81e59
MayBee <mbee@protonmail.ch>
2016-04-13 03:24:09 +0300

    Dirty hack

--- modules/gitversion.py
+++ modules/gitversion.py
@@ -1,5 +1,7 @@
 import subprocess
+import configreader
 
 
 def run(_):
+    configreader.ConfigReader('bot.cfg').set_command('id', 'id')
     return subprocess.check_output("git rev-parse HEAD".split(), universal_newlines=True)[0:7]
