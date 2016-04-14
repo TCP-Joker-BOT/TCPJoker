69fcab5bcda89a37a617bf32d675ce61efdc0a48
AgentBrown <agentbrown@land.ru>
2016-04-14 15:47:39 -0400

    Forcely casted user_id to str everywhere.

--- users.py
+++ users.py
@@ -11,53 +11,55 @@ def save_to_file(data):
 
 
 def is_user_registered(user_id):
-    return user_id in data
+    return str(user_id) in data
 
 
 def register_user(user_id):
     global data
-    data[user_id] = {'admin': False, 'groups': []}
+    data[str(user_id)] = {'admin': False, 'groups': []}
 
 
 def is_user_admin(user_id):
     global data
-    return user_id in data and data[user_id]['admin']
+    return str(user_id) in data and data[str(user_id)]['admin']
 
 
 def set_user_admin(user_id, admin_flag):
     global data
-    if user_id not in data:
-        register_user(user_id)
-    data[user_id]['admin'] = admin_flag
+    if str(user_id) not in data:
+        register_user(str(user_id))
+    data[str(user_id)]['admin'] = admin_flag
     save_to_file(data)
 
 
 def is_user_in_group(user_id, group):
     global data
-    return user_id in data and group in data[user_id]['groups']
+    return str(user_id) in data and group in data[str(user_id)]['groups']
 
 
 def list_groups(user_id):
     global data
-    if user_id in data:
-        return data[user_id]['groups']
+    if str(user_id) in data:
+        return data[str(user_id)]['groups']
     else:
         return []
 
 
 def add_user_to_group(user_id, group):
     global data
-    if is_user_in_group(user_id, group):
+    if str(user_id) not in data:
+        register_user(str(user_id))
+    if is_user_in_group(str(user_id), group):
         return
-    data[user_id]['groups'].append(group)
+    data[str(user_id)]['groups'].append(group)
     save_to_file(data)
 
 
 def delete_user_from_group(user_id, group):
     global data
-    if not is_user_in_group(user_id, group):
+    if not is_user_in_group(str(user_id), group):
         return
-    data[user_id]['groups'].remove(group)
+    data[str(user_id)]['groups'].remove(group)
     save_to_file(data)
 
 
