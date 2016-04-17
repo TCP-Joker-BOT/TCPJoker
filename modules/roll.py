87b63e88fc399ac9b6f492d3b55ecedb35e37ded
AgentBrown <agentbrown@land.ru>
2016-04-17 17:55:46 +0000

    Added roll module

---
+++ modules/roll.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+
+def run(message):
+    args = message['text'].slpit()[1:]
+    if len(args) == 0:
+        return str(random.randint(1, 6))
+    elif len(args) == 1 and args[0].isdigit():
+        return str(random.randint(1, int(args[0])))
+    return 'Incorrect syntax, see /help for help"
