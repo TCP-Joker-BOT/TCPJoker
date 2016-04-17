782ac07a1f8798ec4faecf39a5a39fde8ade8ec7
AgentBrown <agentbrown@land.ru>
2016-04-17 13:58:29 +0000

    Added documentation to users lib

--- users.py
+++ users.py
@@ -8,26 +8,57 @@ DATA_FILE_NAME = 'users.json'
 
 
 def save_to_file(data):
+    """Saves python data to JSON file named :py:const:`DATA_FILE_NAME`.
+    
+    Args:
+        data (dict): Python dict to save
+    """
     f = open(DATA_FILE_NAME, 'w')
     json.dump(data, f)
     f.close()
 
 
 def is_user_registered(user_id):
+    """Checks if user registered in system
+
+    Args:
+        user_id (int): Telegram id of user to check
+
+    Returns:
+        bool: Check result
+    """
     return str(user_id) in data
 
 
 def register_user(user_id):
+    """Registers user with id `user_id` in system. By default, `admin` is False and there are no groups
+
+    Args:
+        user_id (int): Telegram id of user to register
+    """
     global data
     data[str(user_id)] = {'admin': False, 'groups': []}
 
 
 def is_user_admin(user_id):
+    """Checks if user admin or not
+
+    Args:
+        user_id (int): Telegram id of user to check
+    Returns:
+        bool: Check result
+    """
     global data
     return str(user_id) in data and data[str(user_id)]['admin']
 
 
 def set_user_admin(user_id, admin_flag):
+    """Sets user admin status
+
+    Args:
+        user_id (int): Telegram id of user to change
+        admin_flag (bool): New state of admin flag
+    """
     global data
     if str(user_id) not in data:
         register_user(str(user_id))
@@ -36,11 +67,26 @@ def set_user_admin(user_id, admin_flag):
 
 
 def is_user_in_group(user_id, group):
+    """Checks whether user in group
+
+    Args:
+        user_id (int): Telegram id of user to check
+        group (str): Name of group
+    Returns:
+        bool: Check result
+    """
     global data
     return str(user_id) in data and group in data[str(user_id)]['groups']
 
 
 def list_groups(user_id):
+    """List groups user members in
+
+    Args:
+        user_id (int): Telegram id of user to check
+    Returns:
+        list: List of groups
+    """
     global data
     if str(user_id) in data:
         return data[str(user_id)]['groups']
@@ -49,6 +95,12 @@ def list_groups(user_id):
 
 
 def add_user_to_group(user_id, group):
+    """Add user to specified group
+
+    Args:
+        user_id (int): Telegram id of user to add to group
+        group (str): The name of group
+    """
     global data
     if str(user_id) not in data:
         register_user(str(user_id))
@@ -59,6 +111,12 @@ def add_user_to_group(user_id, group):
 
 
 def delete_user_from_group(user_id, group):
+    """Delete user from specified group
+
+    Args:
+        user_id (int): Telegram id of user to delete from group
+        group (str): The name of group
+    """
     global data
     if not is_user_in_group(str(user_id), group):
         return
