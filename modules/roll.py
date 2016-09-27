08270bc6f2abe496a5091016eea97e8d44653c19
MayBee <mbee@protonmail.ch>
2016-09-27 20:21:56 +0300

    Fix /roll 0

--- modules/roll.py
+++ modules/roll.py
@@ -11,6 +11,6 @@ def run(message):
     args = message['text'].split()[1:]
     if len(args) == 0:
         return str(random.randint(1, 6))
-    elif len(args) == 1 and args[0].isdigit():
+    elif len(args) == 1 and args[0].isdigit() and int(args[0]) > 0:
         return str(random.randint(1, int(args[0])))
     return 'Incorrect syntax, see /help for help'
